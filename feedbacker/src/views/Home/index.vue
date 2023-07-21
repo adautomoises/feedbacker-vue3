6944ecb4110810ea2beba0cd98d9a1f99ddd650b
adautomoises <adautomaleandro@gmail.com>
2023-07-21 12:28:44 -0300

    commit inicial

---
+++ feedbacker/src/views/Home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <custom-header 
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2023</p>
+  </div>
+</template>
+
+<script lang="ts">
+  import { onMounted } from 'vue'
+  import { useRouter } from 'vue-router'
+  import CustomHeader from './CustomHeader.vue'
+  import Contact from './Contact.vue'
+
+  export default {
+    components: { CustomHeader, Contact},
+    setup(){
+      const router = useRouter();
+
+      onMounted(() => {
+        const token = window.localStorage.getItem('@feedbacker:token')
+        if(token){
+          router.push({name: 'feedbacks'})
+        }
+      })
+
+      function handleLogin(){
+      }
+      function handleAccountCreate(){
+      }
+
+      return {
+        handleLogin,
+        handleAccountCreate
+      }
+    }
+  }
+</script>
